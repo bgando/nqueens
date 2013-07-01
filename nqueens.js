680ad4ee247cc552035608e6e0fb36157f8c2db2
Bianca <gando.bia@gmail.com>
2013-07-01 14:35:51 -0700

    initial commit

---
+++ nqueens.js
@@ -0,0 +1,109 @@
+  var helpers = { 
+  rows: function(){
+    return _(_.range(this.get('n'))).map(function(rowIndex){
+      return this.get(rowIndex);
+    }, this);
+  },
+
+  togglePiece: function(rowIndex, colIndex){
+    this.get(rowIndex)[colIndex] = + !this.get(rowIndex)[colIndex];
+    this.trigger('change');
+  },
+
+  _getFirstRowColumnIndexForMajorDiagonalOn: function(rowIndex, colIndex){
+    return colIndex - rowIndex;
+  },
+
+  _getFirstRowColumnIndexForMinorDiagonalOn: function(rowIndex, colIndex){
+    return colIndex + rowIndex;
+  },
+
+
+  hasAnyRooksConflicts: function(){
+    return this.hasAnyRowConflicts() || this.hasAnyColConflicts();
+  },
+
+  hasAnyQueenConflictsOn: function(rowIndex, colIndex){
+    return (
+      this.hasRowConflictAt(rowIndex) ||
+      this.hasColConflictAt(colIndex) ||
+      this.hasMajorDiagonalConflictAt(this._getFirstRowColumnIndexForMajorDiagonalOn(rowIndex, colIndex)) ||
+      this.hasMinorDiagonalConflictAt(this._getFirstRowColumnIndexForMinorDiagonalOn(rowIndex, colIndex))
+    );
+  },
+
+  hasAnyQueensConflicts: function(){
+    return this.hasAnyRooksConflicts() || this.hasAnyMajorDiagonalConflicts() || this.hasAnyMinorDiagonalConflicts();
+  },
+
+  _isInBounds: function(rowIndex, colIndex){
+    return (
+      0 <= rowIndex && rowIndex < this.get('n') &&
+      0 <= colIndex && colIndex < this.get('n')
+    );
+  },
+
+
+  // todo: fill in all these functions - they'll help you!
+
+  hasRowConflictAt: function(rowIndex){
+    return false; // fixme
+  },
+
+  hasAnyRowConflicts: function(){
+    return false; // fixme
+  },
+
+  hasColConflictAt: function(colIndex){
+    return false; // fixme
+  },
+
+  hasAnyColConflicts: function(){
+    return false; // fixme
+  },
+
+  hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow){
+    return false; // fixme
+  },
+
+  hasAnyMajorDiagonalConflicts: function(){
+    return false; // fixme
+  },
+
+  hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow){
+    return false; // fixme
+  },
+
+  hasAnyMinorDiagonalConflicts: function(){
+    return false; // fixme
+  }
+};
+
+
+window.findNRooksSolution = function(n){
+  var solution = undefined; //fixme
+
+  console.log('Single solution for ' + n + ' rooks:', solution);
+  return solution;
+};
+
+window.countNRooksSolutions = function(n){
+  var solutionCount = undefined; //fixme
+
+  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  return solutionCount;
+};
+
+window.findNQueensSolution = function(n){
+  var solution = undefined; //fixme
+
+  console.log('Single solution for ' + n + ' queens:', solution);
+  return solution;
+};
+
+window.countNQueensSolutions = function(n){
+  var solutionCount = undefined; //fixme
+
+  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
+  return solutionCount;
+};
